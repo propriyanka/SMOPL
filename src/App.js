42beeac77a603d1aaf074250fcaf8d65956004f1
propriyanka <priyankaw483@gmail.com>
2023-08-20 12:54:00 +0530

    smopl

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing Routes and Route
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Main from "./components/Main/Main";
+import Process from "./components/Process/Process";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header /> 
+        <Main/>
+        <Process/>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Main/Main.js
@@ -0,0 +1,79 @@
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import image1 from '../../assets/Images/image1.jpg';
+import image2 from '../../assets/Images/image2.jpg';
+import image3 from '../../assets/Images/image3.jpg';
+import image4 from '../../assets/Images/image4.jpg';
+
+const Slide = ({ image, title, description }) => {
+  return (
+    <div className="relative overflow-hidden group ">
+      <div className="flex">
+        <div className="w-full">
+          <div className="h-full">
+            <img
+              src={image}
+              alt={title}
+              className="object-cover w-full h-[850px] transform scale-110 transition-transform group-hover:scale-100"
+            />
+          </div>
+        </div>
+        <div className="w-1/2 bg-stone-900 p-8">
+          <h2 className="text-3xl font-bold mb-4 text-white">{title}</h2>
+          <p className="text-lg text-white mb-8">{description}</p>
+          <button className="text-lg border text-white rounded-full border-white px-6 py-2 hover:bg-transparent hover:border-gray-300">
+            Contact
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Main = () => {
+  const slides = [
+    {
+        image: image1,
+        title: 'Architecture and Interior Consultancy',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        image: image2,
+        title: 'Designing Spaces for You',
+        description: 'Suspendisse tristique nulla eget elit pretium interdum.',
+      },
+      {
+        image: image3,
+        title: 'Transforming Dreams into Reality',
+        description: 'Fusce at ligula eu sem volutpat blandit eu sit amet ex.',
+      },
+      {
+        image: image4,
+        title: 'Creating Functional Spaces',
+        description: 'Aenean et magna sed lectus consectetur fermentum.',
+      },
+    ];
+
+  return (
+    <div>
+      <div className="mt-0">
+        <Carousel
+          showThumbs={false}
+          showStatus={false}
+          showIndicators={false}
+          showArrows={true}
+          autoPlay
+          infiniteLoop
+        >
+          {slides.map((slide, index) => (
+            <Slide key={index} {...slide} />
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
